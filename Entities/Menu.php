2a310eb5f54cc3281273494a0aa048ec771b39d4
Cristian Marin <marin101998@gmail.com>
2023-05-19 11:20:51 -0500

    +add | revisionable trait

--- Entities/Menu.php
+++ Entities/Menu.php
@@ -6,29 +6,32 @@ use Astrotomic\Translatable\Translatable;
 use Illuminate\Database\Eloquent\Model;
 use Modules\Isite\Entities\Module;
 use Stancl\Tenancy\Database\Concerns\BelongsToTenant;
+use Modules\Isite\Traits\RevisionableTrait;
 
 use Modules\Core\Support\Traits\AuditTrait;
 
 class Menu extends Model
 {
-    use Translatable, BelongsToTenant, AuditTrait;
+  use Translatable, BelongsToTenant, AuditTrait, RevisionableTrait;
 
-    protected $fillable = [
-        'name',
-        'title',
-        'status',
-        'primary',
-    ];
-    public $translatedAttributes = ['title', 'status'];
-    protected $table = 'menu__menus';
+  public $repository = 'Modules\Menu\Repositories\MenuRepository';
 
-    public function menuitems()
-    {
-        $modulesEnabled = implode("|",Module::where("enabled",1)->get()->pluck("alias")->toArray() ?? []);
-        
-        $relation = $this->hasMany('Modules\Menu\Entities\Menuitem')->with("translations")->orderBy('position', 'asc');
-        $relation->whereRaw("system_name REGEXP '$modulesEnabled'");
-        
-        return $relation;
-    }
+  protected $fillable = [
+    'name',
+    'title',
+    'status',
+    'primary',
+  ];
+  public $translatedAttributes = ['title', 'status'];
+  protected $table = 'menu__menus';
+
+  public function menuitems()
+  {
+    $modulesEnabled = implode("|", Module::where("enabled", 1)->get()->pluck("alias")->toArray() ?? []);
+
+    $relation = $this->hasMany('Modules\Menu\Entities\Menuitem')->with("translations")->orderBy('position', 'asc');
+    $relation->whereRaw("system_name REGEXP '$modulesEnabled'");
+
+    return $relation;
+  }
 }
