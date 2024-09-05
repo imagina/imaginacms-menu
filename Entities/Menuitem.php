7988cc26fef6398866db9eb604f5929db72317c5
michael <msolano2505@gmail.com>
2024-09-05 16:34:53 -0500

    Fixed issues with the menuitems relation

--- Entities/Menuitem.php
+++ Entities/Menuitem.php
@@ -10,89 +10,94 @@ use TypiCMS\NestableTrait;
 
 class Menuitem extends CrudModel
 {
-    use Translatable, NestableTrait, BelongsToTenant;
+  use Translatable, NestableTrait, BelongsToTenant;
 
-    protected $table = 'menu__menuitems';
-    public $transformer = 'Modules\Menu\Transformers\MenuitemTransformer';
-    public $repository = 'Modules\Menu\Repositories\MenuItemRepository';
-    public $requestValidation = [
-        'create' => 'Modules\Menu\Http\Requests\CreateMenuItemRequest',
-        'update' => 'Modules\Menu\Http\Requests\UpdateMenuItemRequest',
-    ];
-    //Instance external/internal events to dispatch with extraData
-    public $dispatchesEventsWithBindings = [
-        //eg. ['path' => 'path/module/event', 'extraData' => [/*...optional*/]]
-        'created' => [],
-        'creating' => [],
-        'updated' => [],
-        'updating' => [],
-        'deleting' => [],
-        'deleted' => []
-    ];
+  protected $table = 'menu__menuitems';
+  public $transformer = 'Modules\Menu\Transformers\MenuitemTransformer';
+  public $repository = 'Modules\Menu\Repositories\MenuItemRepository';
+  public $requestValidation = [
+    'create' => 'Modules\Menu\Http\Requests\CreateMenuItemRequest',
+    'update' => 'Modules\Menu\Http\Requests\UpdateMenuItemRequest',
+  ];
+  //Instance external/internal events to dispatch with extraData
+  public $dispatchesEventsWithBindings = [
+    //eg. ['path' => 'path/module/event', 'extraData' => [/*...optional*/]]
+    'created' => [],
+    'creating' => [],
+    'updated' => [],
+    'updating' => [],
+    'deleting' => [],
+    'deleted' => []
+  ];
 
-    public $translatedAttributes = ['title', 'uri', 'url', 'status', 'locale', 'description'];
+  public $translatedAttributes = ['title', 'uri', 'url', 'status', 'locale', 'description'];
 
-    protected $fillable = [
-        'menu_id',
-        'page_id',
-        'system_name',
-        'parent_id',
-        'position',
-        'target',
-        'module_name',
-        'is_root',
-        'icon',
-        'link_type',
-        'class',
-    ];
+  protected $fillable = [
+    'menu_id',
+    'page_id',
+    'system_name',
+    'parent_id',
+    'position',
+    'target',
+    'module_name',
+    'is_root',
+    'icon',
+    'link_type',
+    'class',
+  ];
 
-    /**
-     * For nested collection
-     *
-     * @var array
-     */
-    public $children = [];
+  /**
+   * For nested collection
+   *
+   * @var array
+   */
+  public $children = [];
 
-    public function menu()
-    {
-        return $this->belongsTo(Menu::class);
-    }
+  public function menu()
+  {
+    return $this->belongsTo(Menu::class);
+  }
 
-    /**
-     * Make the current menu item child of the given root item
-     */
-    public function makeChildOf(Menuitem $rootItem)
-    {
-        $this->parent_id = $rootItem->id;
-        $this->save();
-    }
+  /**
+   * Make the current menu item child of the given root item
+   */
+  public function makeChildOf(Menuitem $rootItem)
+  {
+    $this->parent_id = $rootItem->id;
+    $this->save();
+  }
 
-    /**
-     * Check if the current menu item is the root
-     */
-    public function isRoot()
-    {
-        return (bool) $this->is_root;
-    }
+  /**
+   * Check if the current menu item is the root
+   */
+  public function isRoot()
+  {
+    return (bool)$this->is_root;
+  }
 
-    /**
-     * Check if page_id is empty and returning null instead empty string
-     */
-    public function setPageIdAttribute($value)
-    {
-        $this->attributes['page_id'] = ! empty($value) ? $value : null;
-    }
+  /**
+   * Check if page_id is empty and returning null instead empty string
+   */
+  public function setPageIdAttribute($value)
+  {
+    $this->attributes['page_id'] = !empty($value) ? $value : null;
+  }
 
-    /**
-     * Check if parent_id is empty and returning null instead empty string
-     */
-    public function setParentIdAttribute($value)
-    {
-        $this->attributes['parent_id'] = ! empty($value) ? $value : null;
-    }
+  /**
+   * Check if parent_id is empty and returning null instead empty string
+   */
+  public function setParentIdAttribute($value)
+  {
+    $this->attributes['parent_id'] = !empty($value) ? $value : null;
+  }
 
-    public function setSystemNameAttribute($value)
-    {
-        $this->attributes['system_name'] = ! empty($value) ? $value : \Str::slug($this->title, '-');
-    }
+  public function getParentIdAttribute($value)
+  {
+    return intval(is_null($value) ? 0 : $value);
+  }
+
+  public function setSystemNameAttribute($value)
+  {
+    $this->attributes['system_name'] = !empty($value) ? $value : \Str::slug($this->title, '-');
+  }
 }
--- Transformers/MenuTransformer.php
+++ Transformers/MenuTransformer.php
@@ -7,40 +7,13 @@ use Modules\Core\Icrud\Transformers\CrudResource;
 
 class MenuTransformer extends CrudResource
 {
-
-    /**
-     * Method to merge values with response
-     *
-     * @return array
-     */
-    public function modelAttributes($request)
-    {
-        return [
-            'menuitems' => $this->getMenuItems(),
-        ];
-    }
-
-    /*
-    * Integration with tenant - menuitems
-    */
-    public function getMenuItems()
-    {
-        return Cache::store(config('cache.default'))->remember('menu_items_' . $this->id . (tenant()->id ?? ''), 60, function () {
-            $params = [
-                'include' => [],
-                'filter' => [
-                    'menu' => $this->id,
-                    'order' => ['way' => 'asc'],
-                ],
-            ];
-
-            $menuItems = app('Modules\Menu\Repositories\MenuItemRepository')->getItemsBy(json_decode(json_encode($params)));
-
-            if (!empty($menuItems)) {
-                return MenuitemTransformer::collection($menuItems);
-            }
-
-            return '';
-        });
-    }
+  /**
+   * Method to merge values with response
+   *
+   * @return array
+   */
+  public function modelAttributes($request)
+  {
+    return [];
+  }
 }
